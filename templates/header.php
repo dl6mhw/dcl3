1230d150e7e9347d0b2e910c582cc615654533b2
DL6MHW <hoeding@th-brandenburg.de>
2025-11-23 23:14:02 +0100

    Kosmetik in header und footer2

--- templates/header.php
+++ templates/header.php
@@ -98,7 +98,7 @@
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <img src="../media/0_DCL_logo.png" width=60 height=40><li><a href="#" class="nav-link px-2 text-white">Home</a></li>
-		  <h2>DCL3 = darc CONTEST logbuch</h2>
+		  <h2 style="border:2px solid white">DCL3 = darcCONTESTlogbuch</h2>
           <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
           <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
           <li><a href="#" class="nav-link px-2 text-white">About</a></li>
